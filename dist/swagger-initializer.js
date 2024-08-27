a96b3530617eb8435196fca7c5d32c2eacaae0ef
Mohammad Fereydouni <mfereydouni@cii.continental-ins.com>
2024-08-27 15:39:00 -0400

    Updated API Gateway Swagger with Acord 103, 1126, 203

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   const ui = SwaggerUIBundle({
-    url: "https://t6t2a50kfi.execute-api.us-east-1.amazonaws.com/dev/swagger",
+    url: "https://l7sf9cy018.execute-api.us-east-1.amazonaws.com/dev/swagger",
     dom_id: '#swagger-ui',
     presets: [
       SwaggerUIBundle.presets.apis,
