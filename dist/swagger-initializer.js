b3e09f4ec04c8af5661336684326e3fda0cb7440
Maf Fereydouni <mfereydouni@cii.continental-ins.com>
2024-10-02 15:03:33 -0600

    fixed the swagger ui bug with index.html and swagger-initializer added agent

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -9,20 +9,25 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout",
     onComplete: function() {
-      // Add custom buttons into Swagger topbar
+      // Check if the buttons already exist before adding them
       const topbar = document.querySelector('.topbar');
-      if (topbar) {
-        // Create buttons
+      const existingButtons = document.getElementById('custom-buttons');
+      
+      if (!existingButtons && topbar) {
+        // Create a container for the buttons
         const buttonContainer = document.createElement('div');
+        buttonContainer.id = 'custom-buttons';
+        buttonContainer.style.display = 'inline-block';
         buttonContainer.innerHTML = `
           <button id="acord-btn" style="margin-right: 10px;">ACORD API</button>
-          <button id="policies-btn">Policies API</button>
+          <button id="policies-btn" style="margin-right: 10px;">Policies API</button>
+          <button id="agent-btn" style="margin-right: 10px;">Agent API</button>
         `;
 
-        // Append buttons to the topbar
+        // Append the buttons only once
         topbar.appendChild(buttonContainer);
 
-        // Add event listeners for the buttons
+        // Add event listeners for buttons
         document.getElementById('acord-btn').addEventListener('click', function() {
           ui.specActions.updateUrl("https://vvqx0s8wpj.execute-api.us-east-1.amazonaws.com/dev/swagger");
           ui.specActions.download("https://vvqx0s8wpj.execute-api.us-east-1.amazonaws.com/dev/swagger");
@@ -32,21 +37,11 @@ window.onload = function() {
           ui.specActions.updateUrl("https://rtq606spbi.execute-api.us-east-1.amazonaws.com/dev/swagger");
           ui.specActions.download("https://rtq606spbi.execute-api.us-east-1.amazonaws.com/dev/swagger");
         });
-<<<<<<< HEAD
-		
-		document.getElementById('agent-btn').addEventListener('click', function() {
-          ui.specActions.updateUrl("https://srfhe708e3.execute-api.us-east-1.amazonaws.com/dev/swagger");
-          ui.specActions.download("https://srfhe708e3.execute-api.us-east-1.amazonaws.com/dev/swagger");
-        });
-		
-=======
 
         document.getElementById('agent-btn').addEventListener('click', function() {
           ui.specActions.updateUrl("https://srfhe708e3.execute-api.us-east-1.amazonaws.com/dev/swagger");
           ui.specActions.download("https://srfhe708e3.execute-api.us-east-1.amazonaws.com/dev/swagger");
         });
-        
->>>>>>> e71f49e06323dbeb9de99f68473d484496f5792d
       }
     }
   });
